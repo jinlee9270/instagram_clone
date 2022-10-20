3605225b1ee72c2a739044487ab2c0c31ce548d6
jinlee <vito9270@naver.com>
2022-10-20 22:49:38 +0900

    가입 로그인 페이지 왔다갔다 완성

--- src/components/login.js
+++ src/components/login.js
@@ -1,42 +1,44 @@
 import React, { useState } from "react"
-
+import { StyledInput, StyledButton, LoginContainer_1, LoginContainer_2, Wrap, Greeting } from "./styledComponent"
 const Login = () => {
-    const [isLogin, setIsLogin] = useState(false)
-    
+    const [isLogin, setIsLogin] = useState(true)
+    const [isEmail, setIsEmail] = useState('')
+    const [isPassword, setIsPassword] = useState('')
+
     return (
-        <div style={{display:'flex', flexDirection: 'column'}}>
-            <div style={{borderStyle:'solid'}}>
-            <div>이거 이미지 태그로 바꾸고 그림 넣기</div>
-            <div>친구들의 사진과 동영상을 보려면 가입하세요.</div>
+        <Wrap>
+            <LoginContainer_1>
+            <img src="../../public/img/instagram.png"/>
             {isLogin && <>
                 <div style={{display:'flex', flexDirection: 'column'}}>
-                    <input placeholder="전화번호, 사용자 이름 또는 이메일"></input>
-                    <input placeholder="비밀번호" type="password"></input>   
+                    <StyledInput placeholder="전화번호, 사용자 이름 또는 이메일"></StyledInput>
+                    <StyledInput placeholder="비밀번호" type="password"></StyledInput>   
                 </div>
-                <button>로그인</button>
-                <div style={{display:'flex', justifyContent:'center'}}><div>------</div><div>또는</div><div>------</div></div>
+                <StyledButton>로그인</StyledButton>
+                <div style={{display:'flex', justifyContent:'center'}}><hr/><div>또는</div><hr/></div>
                 <div>페이스북으로 로그인 a tag로 변경할 것</div>
                 <div>비밀번호를 잊으셨나요? a tag로 변경할 것</div>
             </>}
             {!isLogin && <>
-                <button>페이스북으로 로그인</button>
-                <div style={{display:'flex', justifyContent:'center'}}><div>------</div><div>또는</div><div>------</div></div>
+                <Greeting>친구들의 사진과 동영상을 보려면 가입하세요.</Greeting>
+                <StyledButton>페이스북으로 로그인</StyledButton>
+                <div style={{display:'flex', justifyContent:'center'}}><hr/><div>또는</div><hr/></div>
                 <div style={{display:'flex', flexDirection: 'column'}}>
-                    <input placeholder="휴대폰 번호 또는 이메일 주소"></input>
-                    <input placeholder="성명"></input>
-                    <input placeholder="사용자 이름"></input>
-                    <input placeholder="비밀번호" type="password"></input>   
+                    <StyledInput placeholder="휴대폰 번호 또는 이메일 주소"></StyledInput>
+                    <StyledInput placeholder="성명"></StyledInput>
+                    <StyledInput placeholder="사용자 이름"></StyledInput>
+                    <StyledInput placeholder="비밀번호" type="password"></StyledInput>   
                 </div>
                 <div>저희 서비스를 이용하는 사람이 회원님의 연락처 정보를 Instagram에 업로드했을 수도 있습니다. <div>더 알아보기</div></div>
                 <button>가입</button>
                 
             </>}
-            </div>
-            <div style={{borderStyle:'solid'}}>
-                {isLogin && <div>계정이 없으신가요? <button>가입하기</button></div>}
-                {!isLogin &&<div>계정이 있으신가요? <button>로그인</button></div>}
-            </div>
-        </div>
+            </LoginContainer_1>
+            <LoginContainer_2>
+                {isLogin && <div>계정이 없으신가요? <span onClick={(e) => {setIsLogin(false)}}>가입하기</span></div>}
+                {!isLogin &&<div>계정이 있으신가요? <span onClick={(e) => {setIsLogin(true)}}>로그인</span></div>}
+            </LoginContainer_2>
+        </Wrap>
     )
 }
 
---
+++ src/components/styledComponent.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const StyledInput = styled.input`
+    width: 300px;
+    padding: 5px;
+    margin: 5px 30px;
+    font-size: 1rem;
+`
+export const Greeting = styled.div`
+    font-size: 1.5rem;
+    font-weight: border;
+    color: grey;
+`
+
+export const StyledButton = styled.button`
+    font-weight: border;
+    font-color: white;
+    padding: 5px 100px;
+    border-radius: 5px;
+`
+export const LoginContainer_1 = styled.div`
+    border-style: solid;
+    border-color: grey;
+`
+export const LoginContainer_2 = styled.div`
+    border-style: solid;
+    border-color: grey;
+    margin-top: 30px;
+`
+
+export const Wrap = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+`
