919562f584b36d585cd6868e30f1014d25f44ff1
jinlee <vito9270@naver.com>
2022-10-24 22:49:15 +0900

    nav로 변경

--- src/components/GNB.js
+++ src/components/GNB.js
@@ -1,9 +1,13 @@
 import React from "react"
 import styled from "styled-components"
 
-const NaviWrap = styled.div`
-    display:flex;
-    justify-content: space-between;
+const NaviWrap = styled.nav`
+    display: flex;
+    margin: 20px;
+`
+const StyledLi = styled.li`
+    list-style-type: none;
+    display: inline-block;
 `
 
 const LogoImg = styled.img`
@@ -17,30 +21,44 @@ const NaviBarImg = styled.img`
 
 const SearchWrap = styled.div`
     display:flex;
+    background-color: #DDDDDD;
+    border-radius: 5px;
 `
 
 const SearchImg = styled.img`
     width: 20px;
     margin: 10px;
 `
-
+const SearchInput = styled.input`
+    border: none;
+    background-color: transparent;
+    font-size: 1rem
+`
 
 const GNB = () => {
     return (
         <NaviWrap>
-            <LogoImg src="../img/instagram.png" />
-            <SearchWrap>
-                <SearchImg src='../img/search.png' />
-                <input placeholder="검색"/>
-            </SearchWrap>
-            <div>
-                <NaviBarImg src='../img/home.png' />
-                <NaviBarImg src='../img/paper_plane.png' />
-                <NaviBarImg src='../img/add-document.png' />
-                <NaviBarImg src='../img/compass.png' />
-                <NaviBarImg src='../img/heart.png' />
-                <NaviBarImg src='../img/profile-user.png' />
-            </div>
+            <ul>
+                <StyledLi>
+                    <LogoImg src="../img/instagram.png" />
+                </StyledLi>
+                <StyledLi>
+                    <SearchWrap>
+                        <SearchImg src='../img/search.png' />
+                        <SearchInput placeholder="검색"/>
+                    </SearchWrap>
+                </StyledLi>
+                <StyledLi>
+                    <div>
+                        <NaviBarImg src='../img/home.png' />
+                        <NaviBarImg src='../img/paper_plane.png' />
+                        <NaviBarImg src='../img/add-document.png' />
+                        <NaviBarImg src='../img/compass.png' />
+                        <NaviBarImg src='../img/heart.png' />
+                        <NaviBarImg src='../img/profile-user.png' />
+                    </div>
+                </StyledLi>
+            </ul>
         </NaviWrap>
     )
 }
